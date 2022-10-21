375ec265d983b8cf0d2f52117fde1222bbed86f1
Vespri <kresna.vespri21@gmail.com>
2022-10-21 18:27:07 +0700

    add product Controller go

---
+++ controllers/productController.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"go-jwt/database"
+	"go-jwt/helpers"
+	"go-jwt/models"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateProduct(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	Product := models.Product{}
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Product)
+	} else {
+		c.ShouldBind(&Product)
+	}
+
+	Product.UserID = userID
+
+	err := db.Debug().Create(&Product).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, Product)
+}
--- helpers/jwt.go
+++ helpers/jwt.go
@@ -1,8 +1,14 @@
 package helpers
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+	"strings"
 
-var secretKey = "secret"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secretKey = "rahasia"
 
 func GenerateToken(id uint, email string) string {
 	claims := jwt.MapClaims{
@@ -15,4 +21,29 @@ func GenerateToken(id uint, email string) string {
 	signedToken, _ := parseToken.SignedString([]byte(secretKey))
 
 	return signedToken
+}
+
+func VerifyToken(c *gin.Context) (interface{}, error) {
+	errResponse := errors.New("Sign in to Proceed")
+	headerToken := c.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(headerToken, "Bearer")
+
+	if !bearer {
+		return nil, errResponse
+	}
+
+	stringToken := strings.Split(headerToken, " ")[1]
+
+	token, _ := jwt.Parse(stringToken, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errResponse
+		}
+		return []byte(secretKey), nil
+	})
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, errResponse
+	}
+
+	return token.Claims.(jwt.MapClaims), nil
 }
