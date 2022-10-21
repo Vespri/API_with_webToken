8eb328544506f652ea8e54a5fbcafff31a04e632
Vespri <kresna.vespri21@gmail.com>
2022-10-21 15:36:58 +0700

    add generateToken go

---
+++ helpers/jwt.go
@@ -0,0 +1,18 @@
+package helpers
+
+import "github.com/dgrijalva/jwt-go"
+
+var secretKey = "secret"
+
+func GenerateToken(id uint, email string) string {
+	claims := jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	}
+
+	parseToken := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	signedToken, _ := parseToken.SignedString([]byte(secretKey))
+
+	return signedToken
+}
